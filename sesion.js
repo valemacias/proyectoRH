44adf87150bd51925b56e19c9b3912db6fc4dd9e
valemacias <66277191+valemacias@users.noreply.github.com>
2020-06-03 14:43:17 -0500

    Add files via upload

---
+++ sesion.js
@@ -0,0 +1,18 @@
+<script type="text/javascrit">
+    function validar(){
+    
+    var Usuario = document.getElementById("Usuario").value;
+     var Contraseña = document.getElementById("Contraseña").value;
+    
+    
+    if(Usuario== "biosbag@.com" &&  Contraseña== "recursoshumanos" ){
+        
+        <a href="acceso.html" alt=""> acceso</a>
+    }
+    else{
+        alert("el usuario o contraseña son incorrectos")
+    }
+}
+    
+    
+    
